9ebede3662ae9c3dcbd872194062e455941b4a8d
karenhernandez <khernandez0329@gmail.com>
2023-10-18 15:46:44 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const element = document.getElementById("main")
+main.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.textContent = "Karen is the champion"
+console.log = (newHeader.textContent)
+document.body.append(newHeader)
+
